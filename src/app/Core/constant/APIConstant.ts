2a444db08ad64d3dd6f5f3b626aa423128fc2492
A Venkatesh <avenkateshanand1998@gmail.com>
2024-04-19 16:19:52 +0530

    i have to create a core modules, services, guards etc..

---
+++ src/app/Core/constant/APIConstant.ts
@@ -0,0 +1,9 @@
+export const APIConstant ={
+   GET_ALL_EMPLOYEES:'getEmployee',
+   CREATE_EMPLOYEE:'addEmployee',
+   EMPLOYEE: {
+        GET_ALL_EMPLOYEES:'getEmployee',
+        CREATE_EMPLOYEE:'addEmployee',
+   }
+   
+}
