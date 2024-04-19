ee6dac600b71561f09cd99d1de9bbf7b33349d14
A Venkatesh <avenkateshanand1998@gmail.com>
2024-04-19 16:36:47 +0530

    i have to import multiple pages like Employee, Attendance etc..

---
+++ src/app/Pages/advance/advance.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advance',
+  templateUrl: './advance.component.html',
+  styleUrl: './advance.component.css'
+})
+export class AdvanceComponent {
+
+}
---
+++ src/app/Pages/attendence/attendence.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attendence',
+  templateUrl: './attendence.component.html',
+  styleUrl: './attendence.component.css'
+})
+export class AttendenceComponent {
+
+}
